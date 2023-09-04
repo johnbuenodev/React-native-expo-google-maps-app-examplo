87239a4924029ae2d7c1b134a447e03d7f754181
johnbuenodev <johnbuenodev@gmail.com>
2023-09-04 18:40:10 -0300

    Iniciando o projeto maps - exemplo de uso, add simples mapa com coordenada hardcode, e botoes(vão ser add funcionalidades)

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
+
+import { TouchableOpacity } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
 
+import MapView from 'react-native-maps';
+
 export default function App() {
+
+  function alterMap() {
+
+  }
+
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Projeto Maps</Text>
+      
+      <View style={{ justifyContent:'space-around',margin:10, marginBottom: 32, flexDirection:'row', width:'90%' }}>
+        <TouchableOpacity onPress={() => alterMap()} style={{ width: '48%', borderRadius: 25,backgroundColor: '#1E90FF',justifyContent:'center', alignItems: 'center',borderWidth:2, borderColor: '#4169E1', paddingHorizontal: 16, paddingVertical: 8 }} >
+          <Text style={{ color:'#FFF', fontSize: 18 }}>Assis</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => alterMap()} style={{ width: '48%', borderRadius: 25,backgroundColor: '#1E90FF',justifyContent:'center', alignItems: 'center',borderWidth:2, borderColor: '#4169E1', paddingHorizontal: 16, paddingVertical: 8 }}>
+          <Text style={{ color:'#FFF', fontSize: 18 }}>Londrina</Text> 
+        </TouchableOpacity>
+      </View>
+      
+      <MapView 
+       style={{ width: 350, height: 500 }}
+       region={{ 
+        latitude: -23.5492243,
+        longitude: -46.5813785,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421
+        }}
+      />
+      
     </View>
   );
 }
@@ -17,4 +46,26 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    fontStyle: 'italic',
+    marginBottom: 16
+  },
 });
+
+/*
+
+ <MapView 
+       style={{ width: 350, height: 500 }}
+       initialRegion={{ 
+        latitude: -23.5492243,
+        longitude: -46.5813785,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421
+        }}
+      />
+
+
+
+*/
